def781df19eb892f24f3f32e75f0cc059eb465f0
boxleysadguy-ops <boxleysadguy@gmail.com>
2026-03-21 12:40:44 +0000

    Update gameupdater.bat

--- gameupdater.bat
+++ gameupdater.bat
@@ -1,5 +1,5 @@
 @echo OFF
 color 6
 echo Downloading New update Wait...
-start https://github.com/boxleysadguy-ops/DigDug/
+start https://github.com/boxleysadguy-ops/DigDug/raw/refs/heads/main/Dig-Dug.exe
 pause
